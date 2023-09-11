1b7068be8b407ef7cc2e8ce7646e2e0e7f6ae30b
Chandan sen <chandan2441139@gmail.com>
2023-09-11 14:07:45 +0600

    basic funtionallity done

--- js/deposit.js
+++ js/deposit.js
@@ -1,5 +1,47 @@
 // step 1: targeting button and input filed 
+const depositField = document.getElementById('deposit-field');
+const depositTotal = document.getElementById('deposit-total');
+const blanceTotal = document.getElementById('balance-total');
+const withdrawTotal = document.getElementById('withdraw-total');
+const withdrawField = document.getElementById('withdraw-field');
+
+
 // deposit 
 document.getElementById('btn-deposit').addEventListener('click', () => {
-    console.log('clicking....')
+  // getting value depost input field
+  const depositFieldValue = parseFloat(depositField.value);
+  console.log('inputed-depost', depositFieldValue);
+
+    // showing innertext deposit input field
+  const newdeposit = depositFieldValue;
+  const previousDeposi = parseFloat(depositTotal.innerText);
+//   sum of deposit input and showing deposit blace 
+  const depositsum = newdeposit + previousDeposi;
+  depositTotal.innerText = depositsum;
+  depositField.value = '';
+//   get blance 
+ const previousBlace = blanceTotal.innerText;
+blanceTotal.innerText = parseFloat(previousBlace) + depositsum;
+
+  
+})
+
+
+
+
+// withdraw blance
+
+document.getElementById('btn-withdraw').addEventListener('click', ()=>{
+    // get withdraw amount 
+    const withdrawAmount = parseFloat(withdrawField.value);
+    const previousWidthdraw =  withdrawTotal.innerText;
+    withdrawField.value = ''
+
+    withdrawTotal.innerText = parseFloat(previousWidthdraw) + withdrawAmount;
+    // console.log('withdraw amount', parseFloat(withdrawTotal.innerText));
+
+
+    const blancebeforeWithdraw = blanceTotal.innerText;
+    blanceTotal.innerText = parseFloat(blancebeforeWithdraw) - parseFloat(withdrawTotal.innerText);
+   
 })
